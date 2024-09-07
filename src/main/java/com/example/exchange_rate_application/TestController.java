81899a568acc23ee8f293d5db4d445f00790d9f7
viduraqcomit <vidura@qcomit.com>
2024-09-07 20:10:01 +0530

    Done

--- src/main/java/com/example/exchange_rate_application/TestController.java
+++ src/main/java/com/example/exchange_rate_application/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String getTest(){
-        return "Pipeline and deployment successfully !!!";
+        return "Pipeline and deployment successfully !";
     }
 }
