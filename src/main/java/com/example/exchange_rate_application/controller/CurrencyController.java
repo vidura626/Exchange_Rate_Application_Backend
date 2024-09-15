9b518ca2f681892413ffe9cbae5f0e8d03f3cf71
viduraqcomit <vidura@qcomit.com>
2024-09-15 07:16:29 +0530

    Done

--- src/main/java/com/example/exchange_rate_application/controller/CurrencyController.java
+++ src/main/java/com/example/exchange_rate_application/controller/CurrencyController.java
@@ -1,7 +1,16 @@
 package com.example.exchange_rate_application.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/currency")
+@CrossOrigin("*")
 public class CurrencyController {
+
+    @GetMapping
+    public String getTest(){
+        return "Pipeline and deployment successfully !!! Test Successfully !!!";
+    }
+
 }
