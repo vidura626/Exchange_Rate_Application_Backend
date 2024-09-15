a90ea68eae8ef046ba93ee1047d6ab8200d4886e
viduraqcomit <vidura@qcomit.com>
2024-09-15 06:55:57 +0530

    Done

---
+++ src/main/java/com/example/exchange_rate_application/controller/CurrencyController.java
@@ -0,0 +1,7 @@
+package com.example.exchange_rate_application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/currency")
+public class CurrencyController {
+}
