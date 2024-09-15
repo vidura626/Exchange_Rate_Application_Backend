b21518d77f01a13649bc078ed2374d35216e4bb1
viduraqcomit <vidura@qcomit.com>
2024-09-15 07:22:57 +0530

    Done

--- src/main/java/com/example/exchange_rate_application/controller/CurrencyController.java
+++ src/main/java/com/example/exchange_rate_application/controller/CurrencyController.java
@@ -3,9 +3,11 @@ package com.example.exchange_rate_application.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("api/currency")
 @CrossOrigin("*")
+@RestController
 public class CurrencyController {
 
     @GetMapping
